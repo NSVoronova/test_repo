679308569c5f9949aa6a0e43ed1994e90209b50e
Nadzeya Voranava <nadya.voronova@gmail.com>
2024-03-11 18:22:03 +0100

    feat: add request

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,43 @@
-import { useEffect, useState } from 'react';
-import { groups } from './mocks/groups';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import GroupCard from './components/GroupCard';
 import Filter from './components/Filter';
+import getMockData from './api/api';
 
 const App: React.FC = () => {
-  const [filterGroups, setFilterGroups] = useState(groups);
+  const [groupsArray, setGroupsArray] = useState<Group[]>([]);
   const [filterColor, setFilterColor] = useState('');
   const [filterPrivate, setFilterPrivate] = useState('');
   const [filterFriends, setFilterFriends] = useState('');
 
   useEffect(() => {
-    const newGroups = groups
-      .filter((group) => {
-        if (
-          group.avatar_color === filterColor ||
-          filterColor === '' ||
-          (filterColor === 'none' && !group.avatar_color)
-        ) {
-          return true;
-        }
-      })
-      .filter((group) => {
-        if (
-          filterPrivate === '' ||
-          (filterPrivate === 'closed' && group.closed) ||
-          (filterPrivate === 'opened' && !group.closed)
-        ) {
-          return true;
-        }
-      })
-      .filter((group) => {
-        if (
-          filterFriends === '' ||
-          (filterFriends === 'friends' && group.friends)
-        ) {
-          return true;
-        }
-      });
-    setFilterGroups(newGroups);
-  }, [filterColor, filterPrivate, filterFriends]);
+    const fetchData = async () => {
+      const data = await getMockData();
+      if (data.result === 1 && data.data) {
+        setGroupsArray(data.data);
+      } else {
+        console.error('Ошибка получения данных');
+        setGroupsArray([]);
+      }
+    };
 
-  return (
-    <div>
-      <h1>Список групп</h1>
+    fetchData();
+  }, []);
+
+  const applyFilters = (group: Group) => {
+    return (
+      (filterColor === '' ||
+        filterColor === group.avatar_color ||
+        (filterColor === 'none' && !group.avatar_color)) &&
+      (filterPrivate === '' ||
+        (filterPrivate === 'closed' && group.closed) ||
+        (filterPrivate === 'opened' && !group.closed)) &&
+      (filterFriends === '' || (filterFriends === 'friends' && group.friends))
+    );
+  };
+
+  const renderFilters = () => (
+    <>
       <label>Фильтровать по: </label>
       <Filter type='color' filter={filterColor} filterChange={setFilterColor} />
       <Filter
@@ -56,19 +50,30 @@ const App: React.FC = () => {
         filter={filterFriends}
         filterChange={setFilterFriends}
       />
-      <div>
-        {filterGroups.map((group) => (
-          <GroupCard
-            closed={group.closed}
-            id={group.id}
-            members_count={group.members_count}
-            name={group.name}
-            avatar_color={group.avatar_color}
-            friends={group.friends}
-            key={group.id}
-          />
-        ))}
-      </div>
+    </>
+  );
+
+  return (
+    <div>
+      <h1>Список групп</h1>
+      {renderFilters()}
+      {groupsArray.length ? (
+        <div>
+          {groupsArray.filter(applyFilters).map((group) => (
+            <GroupCard
+              closed={group.closed}
+              id={group.id}
+              members_count={group.members_count}
+              name={group.name}
+              avatar_color={group.avatar_color}
+              friends={group.friends}
+              key={group.id}
+            />
+          ))}
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 };
---
+++ src/api/api.ts
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+
+
+const getMockData: () => Promise<GetGroupsResponse> = async () => {
+  try {
+    const response = await axios.get('./src/mocks/groups.json');
+
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    
+    if (response.data && response.data.length > 0) {
+      return { result: 1, data: response.data };
+    } else {
+      return { result: 0 };
+    }
+  } catch (error) {
+    console.error('Ошибка получения данных:', error);
+    return { result: 0, error: 'Failed to fetch data' };
+  }
+};
+
+export default getMockData;
